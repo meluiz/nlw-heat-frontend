5a4aa82b2777e5474757f071893cd2732c2b67f7
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-10-30 20:52:20 -0300

    feat: styled the home page

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,5 +1,12 @@
 import styled from "styled-components"
 
 export const Root = styled.div`
-  min-height: 0;
+  width: 100%;
+  height: 100%;
+  max-width: 1280px;
+  display: grid;
+  grid-template-columns: 1fr 453px;
+  column-gap: 120px;
+  margin: 0 auto;
+  position: relative;
 `
