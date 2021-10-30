b53b52188bf4cf9a53b01faa8fffad5a4e2bf1de
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-10-30 20:50:46 -0300

    feat: styled the login box component

--- src/components/login/box/styles.ts
+++ src/components/login/box/styles.ts
@@ -1,5 +1,51 @@
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 
-export const Root = styled.div`
-  min-height: 0;
-`
+/* ------| Medias |------ */
+import BannerGirlImage from '@media/banner-girl.png'
+
+export const Root = styled.div`${({ theme }) => css`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 348px 72px 0;
+  text-align: center;
+  background-color: ${theme.colors.background.secondary};
+  background-image: url(${BannerGirlImage});
+  background-repeat: no-repeat;
+  background-position: center -112px;
+
+  strong {
+    font-size: 32px;
+    font-weight: 600;
+    line-height: 36px;
+  }
+
+  @media screen and (min-width: 1536px) {
+    padding: 440px 72px 0;
+    background-position: center top;
+  }
+`}`
+
+export const LinkGithub = styled.a`${({ theme }) => css`
+  height: 56px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 12px;
+  border-radius: 2px;
+  margin-top: 32px;
+  padding: 0 40px;
+  text-transform: uppercase;
+  text-decoration: none;
+  font-size: 14px;
+  font-weight: 800;
+  color: ${theme.colors.text.secondary};
+  background-color: ${theme.colors.background.tertiary};
+
+  &:hover {
+    filter: brightness(.9);
+  }
+`}`
