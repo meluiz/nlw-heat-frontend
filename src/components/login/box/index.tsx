5ed4d4cd1b57acb3d08b555cd6ce72e88c6265cf
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-10-30 20:51:07 -0300

    feat: structured the login box component

--- src/components/login/box/index.tsx
+++ src/components/login/box/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 
+/* ------| Components |------ */
+import { VscGithubInverted } from 'react-icons/vsc'
+
 /* ------| Styles |------ */
-import { Root } from './styles'
+import { LinkGithub, Root } from './styles'
 
 export const LoginBox = () => {
   return (
-    <Root>Hello, World</Root>
+    <Root>
+      <strong>Envie e compartilhe sua mensagem</strong>
+      <LinkGithub href="#">
+        <VscGithubInverted size={24} />
+        Entrar com Github
+      </LinkGithub>
+    </Root>
   )
 }
