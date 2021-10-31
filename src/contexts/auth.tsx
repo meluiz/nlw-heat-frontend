43e9254acea49e59b8c178340a5482033f3c4133
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-10-31 00:00:17 -0300

    passing token in header on signin

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -43,8 +43,9 @@ export const AuthProvider = ({ children }: AuthProviderType) => {
     })
 
     const { token, user } = response.data
-
+    
     localStorage.setItem('@dowhile:token', token)
+    api.defaults.headers.common.authorization = `Bearer ${token}`
     setUser(user)
   }
 
