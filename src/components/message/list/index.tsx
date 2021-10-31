069ae827a214187f56d3257ee31d30c69cd9df1f
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-10-30 21:28:49 -0300

    feat: structured the message list component

--- src/components/message/list/index.tsx
+++ src/components/message/list/index.tsx
@@ -1,10 +1,62 @@
 import React from 'react'
 
+/* ------| Media |------ */
+import LogoImage from '@media/logo.svg'
+
 /* ------| Styles |------ */
-import { Root } from './styles'
+import {
+  Root,
+  LogoFigure,
+  Unordered,
+  ListItem,
+  UserFigure,
+  User,
+  UserAvatar,
+  UserName,
+  Text
+} from './styles'
 
 export const MessageList = () => {
   return (
-    <Root>Hello, World</Root>
+    <Root>
+      <LogoFigure>
+        <img src={LogoImage} alt="DoWhile 2021" />
+      </LogoFigure>
+      <Unordered>
+        <ListItem>
+          <Text>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</Text>
+          <User>
+            <UserAvatar>
+              <UserFigure>
+                <img src="https://github.com/meluiz.png" alt="Luiz Felipe" />
+              </UserFigure>
+            </UserAvatar>
+            <UserName>Luiz Felipe</UserName>
+          </User>
+        </ListItem>
+        <ListItem>
+          <Text>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</Text>
+          <User>
+            <UserAvatar>
+              <UserFigure>
+                <img src="https://github.com/meluiz.png" alt="Luiz Felipe" />
+              </UserFigure>
+            </UserAvatar>
+            <UserName>Luiz Felipe</UserName>
+          </User>
+        </ListItem>
+        <ListItem>
+          <Text>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</Text>
+          <User>
+            <UserAvatar>
+              <UserFigure>
+                <img src="https://github.com/meluiz.png" alt="Luiz Felipe" />
+              </UserFigure>
+            </UserAvatar>
+            <UserName>Luiz Felipe</UserName>
+          </User>
+        </ListItem>
+      </Unordered>
+    </Root>
   )
 }
