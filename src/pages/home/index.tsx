89965060adaa5ac5477801320ccf19366f95adf7
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-10-30 23:29:38 -0300

    checking if user is logged

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,17 +1,24 @@
+import React from "react"
+
+/* ------| Contexts |------ */
+import { AuthContext } from "@src/contexts/auth"
+
 /* ------| Components |------ */
 import { Wrapper } from "@src/ui/layout"
 import { MessageList } from "@src/components/message/list"
 import { LoginBox } from "@src/components/login/box"
+import { MessageForm } from "@src/components/message/form"
 
 /* ------| Styles |------ */
 import { Root } from './styles'
 
 export function Home() {
+  const { user } = React.useContext(AuthContext)
   return (
     <Wrapper>
       <Root>
         <MessageList />
-        <LoginBox />
+        { !!user ? <MessageForm /> : <LoginBox /> }
       </Root>
     </Wrapper>
   )
