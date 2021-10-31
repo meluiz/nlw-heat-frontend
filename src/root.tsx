3cd8e14693b624903a9b063f123f1a70a8c5aae0
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-10-30 23:25:14 -0300

    auth provider added

--- src/root.tsx
+++ src/root.tsx
@@ -2,12 +2,15 @@ import { ThemeProvider } from "styled-components"
 import { GlobalStyle } from "@src/styles/global"
 import { theme } from '@src/styles/theme'
 import { App } from "@src/App"
+import { AuthProvider } from "./contexts/auth"
 
 export const Root = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </AuthProvider>
   )
 }
