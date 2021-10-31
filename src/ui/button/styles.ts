e13f862d2531c2664c206b5598238928191438fe
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-10-31 16:17:53 -0300

    styles: button component styled

--- src/ui/button/styles.ts
+++ src/ui/button/styles.ts
@@ -1,5 +1,49 @@
-import styled from "styled-components"
+import styled, { css, DefaultTheme, FlattenSimpleInterpolation } from "styled-components"
 
-export const Root = styled.button`
-  min-height: 0;
+/* ------| Types |------ */
+type ButtonPropsType = {
+  theme: DefaultTheme
+  variant?: 'primary' | 'secondary'
+}
+
+type ButtonThemeType = (theme: DefaultTheme, variant?: 'primary' | 'secondary') => string | FlattenSimpleInterpolation
+
+/* ------| Theme |------ */
+const buttonTheme: ButtonThemeType = (theme, variant) => {
+  switch (variant) {
+    case 'primary': return `
+      min-height: 56px;
+      border-radius: 2px;
+      padding: 0 40px;
+      text-transform: uppercase;
+      font-size: 14px;
+      font-weight: 900;
+      font-family: ${theme.font.family};
+      color: ${theme.colors.text.secondary};
+      background-color: ${theme.colors.background.secondary};
+
+      &:hover { filter: brightness(.9) }
+    `
+    break;
+    case 'secondary': return css`
+    
+    `
+    default: return css`
+    
+    `
+  }
+}
+
+const DefaultButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 12px;
+  border: 0;
+  background: transparent;
+  cursor: pointer;
 `
+
+export const Root = styled(DefaultButton)<ButtonPropsType>`${({ theme, variant }) => css`
+  ${buttonTheme(theme, variant)}
+`}`
