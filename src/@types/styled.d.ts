1eb36ca7c7ae463e46017f890089a55fef8a93ed
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-10-31 02:24:49 -0300

    structure theme changed

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -4,11 +4,33 @@ declare module 'styled-components' {
       text: {
         primary: string
         secondary: string
+        tone: {
+          50: string
+          100: string
+          200: string
+          300: string
+          400: string
+          500: string
+          600: string
+          700: string
+          800: string
+          900: string
+        }
       }
       background: {
         primary: string
         secondary: string
-        tertiary: string
+        tone: {
+          100: string
+          200: string
+          300: string
+          400: string
+          500: string
+          600: string
+          700: string
+          800: string
+          900: string
+        }
       }
     },
     font: {
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -4,12 +4,34 @@ export const theme: DefaultTheme = {
   colors: {
     text: {
       primary: '#e1e1e6',
-      secondary: '#09090A'
+      secondary: '#09090A',
+      tone: {
+        50: '#999898',
+        100: '#777676',
+        200: '#6D6D6D',
+        300: '#646363',
+        400: '#5B595A',
+        500: '#5B595A',
+        600: '#484646',
+        700: '#3F3C3D',
+        800: '#363233',
+        900: '#1C191A'
+      }
     },
     background: {
-      primary: '#121214',
-      secondary: '#17171A',
-      tertiary: '#ffcd1e'
+      primary: '#ff008E',
+      secondary: '#FFCD1E',
+      tone: {
+        100: '#040303',
+        200: '#070606',
+        300: '#0B090A',
+        400: '#0E0C0D',
+        500: '#121010',
+        600: '#151313',
+        700: '#191616',
+        800: '#1C191A',
+        900: '#363233'
+      }
     }
   },
   font: {
